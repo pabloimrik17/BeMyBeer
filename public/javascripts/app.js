549bf70471136e8db588fb3f91197e045fac1df7
Pablo <pabloimrik17@gmail.com>
2016-12-21 23:56:54 +0100

    Commit Inicial

---
+++ public/javascripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Etherless-Nxzt on 18/12/2016.
+ */
+var app = angular.module('angularjsNodejsTutorial',[]);
+app.controller('myController', function($scope, $http) {
+    $scope.data = [];
+    var request = $http.get('/data');
+    request.success(function(data) {
+        $scope.data = data;
+    });
+    request.error(function(data){
+        console.log('Error: ' + data);
+    });
+});
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path');
+
+/* GET home page. */
+
+
+
+router.get('/data', function(req,res){
+    res.json([{"id": 1, "name": "Mymm", "city": "Pantano do Sul"},
+        {"id": 2, "name": "Skyble", "city": "Guilmaro"},
+        {"id": 3, "name": "Tagfeed", "city": "Gnosjö"},
+        {"id": 4, "name": "Realcube", "city": "Jrashen"},
+        {"id": 5, "name": "Bluejam", "city": "Zhangjiawo"},
+        {"id": 6, "name": "Jayo", "city": "Obonoma"},
+        {"id": 7, "name": "Cogidoo", "city": "Sungsang"},
+        {"id": 8, "name": "Avavee", "city": "Diawara"},
+        {"id": 9, "name": "Tagtune", "city": "Monywa"},
+        {"id": 10, "name": "Centimia", "city": "Retkovci"}]);
+});
+
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+
+module.exports = router;
