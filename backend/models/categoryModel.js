fda467bd74f9d135aec631a64def6b9746f53913
Pablo <pabloimrik17@gmail.com>
2017-01-06 18:55:19 +0100

    Category Router Acabado

--- backend/models/categoryModel.js
+++ backend/models/categoryModel.js
@@ -1,11 +1,11 @@
-var dataBaseModel  = require("./../dbModel/database");
+var dataBaseModel  = require("../../config/database");
 
 var categoryModel = {};
 
 categoryModel.getCategory = function(id, cb) {
     if(dataBaseModel) {
         var query = "SELECT * " +
-                    "FROM categoryList " +
+                    "FROM category " +
                     "WHERE id = ?";
         dataBaseModel.query(query, id, function(err, result) {
             if(err) {
@@ -17,10 +17,10 @@ categoryModel.getCategory = function(id, cb) {
     }
 };
 
-categoryModel.getAllCategories = function(id, cb) {
+categoryModel.getAllCategories = function(cb) {
     if(dataBaseModel) {
         var query = "SELECT * " +
-                    "FROM categoryList";
+                    "FROM category ";
         dataBaseModel.query(query, function(err, result) {
             if(err) {
                 cb(1001, "Error al obtener todas las categorias");
@@ -31,12 +31,12 @@ categoryModel.getAllCategories = function(id, cb) {
     }
 };
 
-categoryModel.getCategoriesByParent = function(idParent, cb) {
+categoryModel.getCategoriesByParent = function(idCategoryParent, cb) {
     if(dataBaseModel) {
         var query = "SELECT * " +
-                    "FROM categoryList " +
+                    "FROM category " +
                     "WHERE idParent = ?";
-        dataBaseModel.query(query, idParent, function(err, result) {
+        dataBaseModel.query(query, idCategoryParent, function(err, result) {
             if(err) {
                 cb(1002, "Error al obtener todas las categorias por padre");
             } else {
@@ -50,7 +50,7 @@ categoryModel.getCategoriesByParent = function(idParent, cb) {
 
 categoryModel.insertCategory = function(categoryData, cb) {
     if(dataBaseModel) {
-        var query = "INSERT INTO categoryList " +
+        var query = "INSERT INTO category " +
                     "SET ?";
         dataBaseModel.query(query, categoryData, function(err, result) {
             if(err) {
@@ -62,27 +62,33 @@ categoryModel.insertCategory = function(categoryData, cb) {
     }
 };
 
-categoryModel.updateCategory = function(categoryData, cb) {
-    if(dataBaseModel) {
-        var query = "UPDATE categoryList " +
-                    "SET name = ?, " +
-                    "idParent = ?";
-        var values = [categoryData.name, categoryData.idParent];
-        dataBaseModel.query(query, values, function(err, result) {
-            if(err) {
-                cb(1004, "Error al actualizar la categoria");
-            } else {
-                cb(null, result);
-            }
-        });
+categoryModel.updateCategory = function(idCategory, categoryData, cb) {
+    if(idCategory == 1) {
+        cb(1006, "Error, no se puede modificar la categoria root");
+    } else {
+        if (dataBaseModel) {
+            var query = "UPDATE category " +
+                "SET name = ?, " +
+                "idParent = ? " +
+                "WHERE Id = ? ";
+            var values = [categoryData.name, categoryData.idParent, idCategory];
+            dataBaseModel.query(query, values, function (err, result) {
+                if (err) {
+                    cb(1004, "Error al actualizar la categoria");
+                } else {
+                    cb(null, result);
+                }
+            });
+        }
     }
 };
 
-categoryModel.deleteCategory = function(id, cb) {
+categoryModel.deleteCategory = function(idCategory, cb) {
     if(dataBaseModel) {
-        var query = "DELETE FROM categoryList" +
+        var query = "DELETE FROM category " +
                     "WHERE id = ?";
-        dataBaseModel.query(query, id, function(err, result) {
+        console.log(idCategory);
+        dataBaseModel.query(query,[idCategory], function(err, result) {
             if(err) {
                 cb(1005, "Error al borrar la categoria");
             } else {
--- backend/routes/category.js
+++ backend/routes/category.js
@@ -8,5 +8,94 @@ var express = require('express');
 var router = express.Router();
 var categoryModel = require("../models/categoryModel");
 
+router.get("/getCategory/:id", function(req, res) {
+    var idCategory = req.params.id;
+
+    categoryModel.getCategory(idCategory, function(err, result) {
+        if(!err) {
+            res.json(result);
+        } else {
+            res
+                .status(400)
+                .json({err : err, description: result})
+        }
+    })
+});
+
+router.get("/getAllCategories", function(req, res) {
+    categoryModel.getAllCategories(function(err, result) {
+        if(!err) {
+            res.json(result);
+        } else {
+            res
+                .status(400)
+                .json({err : err, description: result})
+        }
+    })
+});
+
+router.get("/getCategoriesByParent/:idParent", function(req, res) {
+    var idParent = req.params.idParent;
+
+    categoryModel.getCategoriesByParent(idParent, function(err, result) {
+        if(!err) {
+            res.json(result);
+        } else {
+            res
+                .status(400)
+                .json({err : err, description: result})
+        }
+    })
+});
+
+router.post("/insertCategory", function(req, res) {
+    var categoryData = {
+        name : req.body.name,
+        idParent : req.body.idParent
+    };
+
+    categoryModel.insertCategory(categoryData, function(err, result) {
+        if(!err) {
+            console.log(result);
+            res.json(result);
+        } else {
+            res
+                .status(400)
+                .json({err : err, description: result})
+        }
+    })
+});
+
+router.put("/updateCategory/:idCategory", function(req, res) {
+    var categoryData = {
+        name : req.body.name,
+        idParent : req.body.idParent
+    };
+    var idCategory = req.params.idCategory;
+
+    categoryModel.updateCategory(idCategory, categoryData, function(err, result) {
+        if(!err) {
+            res.json(result);
+        } else {
+            res
+                .status(400)
+                .json({err : err, description: result})
+        }
+    })
+});
+
+router.delete("/deleteCategory/:idCategory", function(req, res) {
+    var idCategory = req.params.idCategory;
+
+    categoryModel.deleteCategory(idCategory, function(err, result) {
+        if(!err) {
+            res.json(result);
+        } else {
+            res
+                .status(400)
+                .json({err : err, description: result})
+        }
+    })
+});
 
 module.exports = router;
