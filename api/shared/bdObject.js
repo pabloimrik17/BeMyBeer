5b555e131f4290aea5f622eb00b7023148882861
Etherless <pabloimrik17@gmail.com>
2017-11-28 00:28:24 +0100

    Initial bdObject for handling all db stuff Imported and created conection on server

---
+++ api/shared/bdObject.js
@@ -0,0 +1,58 @@
+// https://www.terlici.com/2015/08/13/mysql-node-express.html
+
+'use strict';
+
+require("dotenv").config();
+
+const mysql = require('mysql');
+const async = require('async');
+
+exports.MODE_TEST = process.env.TEST_ENVIROMENT;
+exports.MODE_PRODUCTION = process.env.PRO_ENVIROMENT;
+
+let state = {
+    pool: null,
+    mode: null,
+};
+
+exports.connect = function (mode, done) {
+    state.pool = mysql.createPool({
+        host: process.env.DATABASE_HOST,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+        database: mode === exports.MODE_PRODUCTION ? process.env.PROD_DATABASE_NAME : process.env.TEST_DATABASE_NAME
+    });
+
+    state.mode = mode;
+    done()
+};
+
+exports.get = function () {
+    return state.pool
+};
+
+exports.fixtures = function (data) {
+    const pool = state.pool;
+    if (!pool) return done(new Error('Missing database connection.'));
+
+    let names = Object.keys(data.tables)
+    async.each(names, function (name, cb) {
+        async.each(data.tables[name], function (row, cb) {
+            let keys = Object.keys(row)
+                , values = keys.map(function (key) {
+                return "'" + row[key] + "'"
+            });
+
+            pool.query('INSERT INTO ' + name + ' (' + keys.join(',') + ') VALUES (' + values.join(',') + ')', cb)
+        }, cb)
+    }, done)
+};
+
+module.exports.drop = function (tables, done) {
+    const pool = state.pool;
+    if (!pool) return done(new Error('Missing database connection.'));
+
+    async.each(tables, function (name, cb) {
+        pool.query('DELETE * FROM ' + name, cb)
+    }, done)
+};
--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 'use strict';
 
 require('dotenv').config();
-
 const express = require('express');
-const mysql = require('mysql');
 const bodyParser = require('body-parser');
+
+const db = require('./api/shared/bdObject');
 
 const app = express();
 
@@ -14,6 +14,9 @@ app.use(process.env.API_ENTRY_POINT, require('./api/routes/routes'));
 
 const port = process.env.PORT || 3000;
 // SERVER RUN
-app.listen(port, () => {
-    console.log(`App listeting on http://localhost:${port}`)
+
+db.connect(process.env.CURRENT_ENVIROMENT, () => {
+    app.listen(port, () => {
+        console.log(`App listeting on http://localhost:${port}`);
+    });
 });
