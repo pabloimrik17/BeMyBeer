f7cc5d783889c393ae8ed7c2cb795dda3457008e
Etherless <pabloimrik17@gmail.com>
2017-12-02 18:00:12 +0100

    New npm packages: --Moment, chai for date type, and faker
    commonjs:
    -- New requires of npm packages
    
    Category Class Unit Test:
    -- For basic CRUD operations
    
    Renamed bdObject to dbObject (english)

--- test/common.js
+++ test/common.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
-const db = require('../api/shared/dbObject');
+
+const db = require('../db/dbObject');
+const chai = require('chai');
+const faker = require('faker/locale/es');
+const moment = require('moment');
+
+const expect = chai.expect;
+chai.use(require('chai-date-string'));
 
 db.connect(process.env.CURRENT_ENVIROMENT, () => {});
-
-const chai = require('chai');
-const expect = chai.expect;
-
+exports.db = db;
 
 exports.expect = expect;
-exports.db = db;
+exports.faker = faker;
+exports.moment = moment;
+
+
