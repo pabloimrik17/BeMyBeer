0c97c93fc57700b1d451d6cea617e4d77a2f0fbd
Etherless <pabloimrik17@gmail.com>
2017-12-13 00:02:55 +0100

    Work in progress (...)

--- test/seeders/category.seeder.js
+++ test/seeders/category.seeder.js
@@ -3,17 +3,38 @@
 const ObjectSeeder = require('./object.seeder');
 const Category = require('../../api/models/category.model');
 
-const _tableName = Category.dbEntity;
+const _definition = {
+    primaryKey: Category.primaryKey,
+    tableName: Category.tableName,
+    dbProperties: Category.dbProperties,
+};
 
 class CategorySeeder extends ObjectSeeder {
 
-    constructor() {
-        super();
+    static get getPrimaryKey() {
+        return _definition.primaryKey;
     }
 
     static get getTableName() {
-        return _tableName;
+        return _definition.tableName;
     }
+
+    static get getDbProperties() {
+        return _definition.dbProperties;
+    }
+
+    static async up() {
+        try {
+            return await _testDb.schema.createTableIfNotExists(Category, (table) => {
+                table.increments("id_category");
+                table.string('name');
+                table.timestamps();
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
 }
 
 module.exports = CategorySeeder;
--- test/seeders/object.seeder.js
+++ test/seeders/object.seeder.js
@@ -3,18 +3,6 @@
 const _testDb = require('../db/database.test');
 
 class ObjectSeeder {
-
-    async up() {
-        try {
-            return await _testDb.schema.createTableIfNotExists("category2", (table) => {
-                table.increments("id_category");
-                table.string('name');
-                table.timestamps();
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
 }
 
 module.exports = ObjectSeeder;
