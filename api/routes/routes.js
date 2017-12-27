34970ec3a81c08b9e8f39858fdb62fb4bfd24771
Pablo <pabloimrik17@hotmail.com>
2017-12-27 18:19:31 +0100

    Routes: -- Fixed names to match correct RESTful syntax.

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,13 +1,11 @@
-'use strict';
-
 const express = require('express');
-const router = express.Router();
-
 // ROUTES
 const categoryRoutes = require('./category.routes');
 const beerRoutes = require('./beer.routes');
 
-router.use('/categories', categoryRoutes);
-router.use('/beers', beerRoutes);
+const router = express.Router();
+
+router.use('/category', categoryRoutes);
+router.use('/beer', beerRoutes);
 
 module.exports = router;
