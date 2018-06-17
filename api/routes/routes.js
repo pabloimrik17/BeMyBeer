52a2a8c34a9e183eb6cecb0bd1a5bbbe4e340269
Etherless <pabloimrik17@gmail.com>
2018-06-17 20:41:13 +0200

    fix

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,13 +1,10 @@
-'use strict';
-
 const express = require('express');
+
 const router = express.Router();
 
 // ROUTES
 const categoryRoutes = require('./category.routes');
 const beerRoutes = require('./beer.routes');
-
-const router = express.Router();
 
 router.use('/category', categoryRoutes);
 router.use('/beer', beerRoutes);
