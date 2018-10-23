18aa804cebefe6a6acc162a7d0f4571df139e694
Etherless <pabloimrik17@gmail.com>
2018-10-23 23:49:24 +0200

    Updated beer routes with the new changes.

--- src/test/unit/classes/objectModel.class.test.ts
+++ src/test/unit/classes/objectModel.class.test.ts
@@ -1,34 +1,22 @@
 import 'reflect-metadata';
 import ObjectModel from '../../../api/classes/ObjectModel.class';
 import * as lodash from 'lodash';
-import Database from '../../../api/shared/Database';
+import { database } from '../../../api/shared/Database';
 
-jest.mock('../../../api/shared/Database');
-/*jest.mock('../../../api/shared/Database', () => {
-    return jest.fn().mockImplementation(() => {
-        return {
-            Pool: jest.fn()
-        };
-    });
-}); */
-
-let objectModel: ObjectModel;
+let objectModel: ObjectModel
 
 describe('Object Model', () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-        objectModel = new ObjectModel(0);
-    });
+  beforeEach(() => {
+    jest.clearAllMocks()
+    objectModel = new ObjectModel(0, database, lodash)
+  })
 
-    test('For Each Should Be Called Two Times', () => {
-        objectModel.prueba();
-        expect(lodash.forEach).toHaveBeenCalledTimes(2);
-    });
+  test('For Each Should Be Called Two Times', () => {
+    objectModel.prueba()
+    expect(lodash.forEach).toHaveBeenCalledTimes(2)
+  })
 
-    test('hola', () => {
-        const database: Database = new Database();
-        const a = database.Pool;
-        expect(typeof database.Pool).toBe('object');
-        expect(database.Pool).toBeTruthy();
-    });
-});
+  test('hola', () => {
+    expect(database.mode).toBe('HOLA')
+  })
+})
