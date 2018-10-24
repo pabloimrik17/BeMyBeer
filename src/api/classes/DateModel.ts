3cb8a8b6ee2acf410edcb189580dfc83653682f1
pfguerra <pabloimrik17@gmail.com>
2018-10-24 18:16:30 +0200

    Date Model test and DI for Moment in class

--- src/api/classes/DateModel.ts
+++ src/api/classes/DateModel.ts
@@ -1,13 +1,22 @@
-import * as importedMoment from 'moment';
+import { inject, injectable } from 'inversify';
+import 'reflect-metadata';
 import { Moment } from '../ioc/interfaces';
 
+import { THIRD_PARTY_TYPES } from '../ioc/THIRD_PARTY_TYPES';
+
+@injectable()
 export default class DateModel {
-    public static readonly DATE_FORMAT: string = 'YYYY-MM-DD HH:mm:ss';
+  public static readonly DATE_FORMAT: string = 'YYYY-MM-DD HH:mm:ss';
 
-    constructor() {
-    }
+  private _moment: Moment;
 
-    public static getCurrentDate(moment: Moment = importedMoment): string {
-        return moment.utc().format(DateModel.DATE_FORMAT);
-    }
+  constructor(@inject(THIRD_PARTY_TYPES.Moment)moment: Moment) {
+    this._moment = moment;
+  }
+
+  public getCurrentDate(): string {
+    const currentDate = this._moment.utc();
+
+    return currentDate.format(DateModel.DATE_FORMAT);
+  }
 }
