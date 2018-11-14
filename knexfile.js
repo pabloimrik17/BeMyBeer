1438ce3e0ee2f9e74602a7f3df00614315d332b4
Etherless <pabloimrik17@gmail.com>
2018-11-15 00:20:56 +0100

    Updated knexfile host config for environments

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
     [process.env.TEST_ENV]: {
         client: 'mysql2',
         connection: {
-            host: process.env.TEST_DATABASE_HOST_IP,
+            host: process.env.TEST_DATABASE_HOST,
             user: process.env.TEST_DATABASE_USER,
             password: process.env.TEST_DATABASE_PASS,
             database: process.env.TEST_DATABASE_NAME,
@@ -17,7 +17,7 @@ module.exports = {
     [process.env.DEV_ENV]: {
         client: 'mysql2',
         connection: {
-            host: process.env.DEV_DATABASE_HOST_IP,
+            host: process.env.DEV_DATABASE_HOST,
             user: process.env.DEV_DATABASE_USER,
             password: process.env.DEV_DATABASE_PASS,
             database: process.env.DEV_DATABASE_NAME,
@@ -26,7 +26,7 @@ module.exports = {
     [process.env.PROD_ENV]: {
         client: 'mysql2',
         connection: {
-            host: process.env.PROD_DATABASE_HOST_IP,
+            host: process.env.PROD_DATABASE_HOST,
             user: process.env.PROD_DATABASE_USER,
             password: process.env.PROD_DATABASE_PASS,
             database: process.env.PROD_DATABASE_NAME,
